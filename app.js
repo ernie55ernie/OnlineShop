96b1ef2da57adab525cbada1a44facc2cb0945ed
ernie <b01505025@ntu.edu.tw>
2015-06-26 18:47:32 +0800

    Algorithm for creating rules and csv to json template

--- app.js
+++ app.js
@@ -116,6 +116,8 @@ app.get('/partials/:name', routes.partials);
 app.get('/api/name', api.name);
 app.get('/api/csv', api.getAllCsv);
 app.post('/api/savecsv', api.saveCsv);
+app.get('/api/csvNumber', api.csvNumber);
+app.get('/api/csvtojson', api.csvToJson);
 
 // Product
 app.get('/getproduct/:pid', product.getProduct);
--- routes/history.js
+++ routes/history.js
@@ -13,3 +13,48 @@ var sequelize = new Sequelize(
 	local.model.mysql.options
 );
 
+exports.getHistory = function(req, res){
+	History.sync().then(function(){
+		History.findAll().then(function(result){
+			res.json(result);
+		})
+	})
+}
+
+exports.getHistory = function(req, res){
+	History.sync().then(function(){
+		History.find({
+			where: {
+				pid: req.pid
+			}
+		}).then(function(result){
+			res.json(result);
+		})
+	})
+}
+
+exports.createHistory = function(req, res){
+	var product = req.body;
+	History.sync().then(function(){
+		History
+		.build(product)
+		.save()
+		.then(function(anotherTask){
+			res.json(anotherTask);
+		})
+	})
+}
+
+exports.deleteHistory = function(req, res){
+	var pid = req.params.pid;
+	History.destroy({
+		where: {
+			pid: pid
+		}
+	}).then(function(){
+		res.json({message: 'History removed'})
+	}).catch(function(err){
+		console.log(err);
+	})
+}
+
